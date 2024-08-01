cea9a12e3cc04f2265330f8bce9ca06690926b03
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-08-01 16:16:48 -0700

    Creando variable reactiva para almacenar la información obtenida del formulario

--- src/views/UserFormView.vue
+++ src/views/UserFormView.vue
@@ -2,31 +2,43 @@
     <div class="container">
         <div class="register-form">
             <el-card class="card">
-                <div class="form">
-                    <p>Ingrese su nombre de usuario</p>
-                    <el-input placeholder="Nombre" />
-                </div>
-                <div class="form">
-                    <p>Ingrese su correo electrónico</p>
-                    <el-input placeholder="Correo electrónico" />
-                </div>
-                <div class="form">
-                    <p>Ingrese el teléfono</p>
-                    <el-input placeholder="Teléfono" />
-                </div>
-                <div class="form">
-                    <p>Ingrese alguna referencia</p>
-                    <el-input placeholder="Referencias" />
-                </div>
-                <div class="form">
-                    <el-button type="primary" size="default"><router-link to="/usuarios">Registrar usuario</router-link></el-button>
-                </div>
+                <Form @submit="saveUser">
+                    <div class="form">
+                        <p>Ingrese su nombre de usuario</p>
+                        <el-input v-model="information[0]" placeholder="Nombre" />
+                    </div>
+                    <div class="form">
+                        <p>Ingrese su correo electrónico</p>
+                        <el-input v-model="information[1]" placeholder="Correo electrónico" />
+                    </div>
+                    <div class="form">
+                        <p>Ingrese el teléfono</p>
+                        <el-input v-model="information[2]" placeholder="Teléfono" />
+                    </div>
+                    <div class="form">
+                        <p>Ingrese alguna referencia</p>
+                        <el-input v-model="information[3]" placeholder="Referencias" />
+                    </div>
+                    <div class="form">
+                        <button type="submit">Registrar usuario</button>
+                        <!-- <el-button type="primary" size="default"><router-link to="/usuarios">Registrar usuario</router-link></el-button> -->
+                    </div>
+                </Form>
             </el-card>
         </div>
     </div>
 </template>
 
 <script setup>
+    import { sendUser } from '@/services/sendUser';
+    import { Form } from 'vee-validate';
+    import { ref } from 'vue';
+    const information = ref([]);
+
+    const saveUser = () => {
+        sendUser(information);    
+    }
+
     
 </script>
 
