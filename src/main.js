7d04c6f3c21f142e70b930d54bed6659f5637d43
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-08-03 01:56:10 -0700

    Obteniendo el token JWT generado en el local storage para usarlo en las demás URL

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios from "axios"
 
-import App from './App.vue'
-import router from './router'
+let token = JSON.parse(localStorage.getItem('token'));
+if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 const app = createApp(App)
 
