ef0d47edea71048c0abefb3db9e422061832031d
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-08-04 19:29:08 -0700

    Modificando el diseño del login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,44 +1,35 @@
 <template>
-    <div v-if="unauthorizedUser">
-        <h1>Lo sentimos, no puede acceder debido a un error en sus datos, verifique de nuevo</h1>
-    </div>
     <div class="container">
-        <div>
-            <h1><strong>Inicio de sesión</strong></h1>
-            <p>
-                Bienvenido al sistema de gestion de usuarios y contactos asignados. Por favor ingrese
-                su nombre de usuario y contraseña para acceder a la aplicación web.
-            </p>
-        </div>
-        <div class="container-login">
-            <Form>
-                <div class="form">
-                    <p>Usuario</p>
-                    <el-input v-model="user" placeholder="Ingrese su usuario" id="usuario"></el-input>
+        <el-card class="card">
+            <div class="information-card">
+                <h1><strong>Inicio de sesión</strong></h1>
+                <p>Bienvenido al sistema de gestion de usuarios y contactos asignados. Por favor ingrese su nombre de usuario y contraseña para acceder a la aplicación web.</p>
+            </div>
+            <div class="login-card">
+                <div class="login-card-children">
+                    <div class="form-login-card">
+                        <label for="username">Ingrese su nombre de usuario</label>
+                        <el-input class="user-input" name="username" type="text" :rows="2" v-model="user" placeholder="Usuario"/>
+                    </div>
+                    <div class="form-login-card">
+                        <label class="email" for="email">Ingrese su correo electrónico</label>
+                        <el-input class="email-input" name="email" type="email" :rows="2" v-model="email" placeholder="Correo electrónico"/>
+                    </div>
+                    <el-button class="button" type="primary" size="default" @click="validateUser">Acceder</el-button>
                 </div>
-                <div class="form">
-                    <p>Email</p>
-                    <el-input v-model="email" type="email" placeholder="Ingrese su email" show-password />
-                </div>
-                <div class="form">
-                    <!-- <button type="submit">Acceder</button> -->
-                    <el-button type="primary" size="default" @click="validateUser">Acceder</el-button>
-                </div>
-            </Form>
-        </div>
+            </div>
+        </el-card>
     </div>
 </template>
 
 <script setup>
     import router from '@/router';
-import { authenticateUser } from '@/services/authenticateUser';
+    import { authenticateUser } from '@/services/authenticateUser';
     import { storageToken } from "@/services/storageToken";
-    import { Form } from 'vee-validate';
     import { ref } from 'vue'
 
     const user = ref('');
     const email = ref('');
-    const unauthorizedUser = ref();
 
     const validateUser = () => {
         authenticateUser(user, email).then(resp => {
@@ -66,17 +57,51 @@ import { authenticateUser } from '@/services/authenticateUser';
 
 <style scoped>
     .container {
-        height: 50vh;
         position: absolute;
         top: 50%;
         right: 50%;
         transform: translate(50%, -50%);
-        border: 1px solid blue;
-        border-radius: 15px;
-        padding: 25px;
     }
 
-    .container > div:first-child {
+    .container > .card {
+        height: 70vh;
+        /* background: black;
+        color: white; */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .information-card {
+        text-align: center;
+    }
+
+    .information-card > h1 {
+        font-size: 40px;
+        font-weight: bold;
+    }
+
+    .information-card > p {
+        font-size: 20px;
+    }
+
+    .login-card {
+        width: 100%;
+    }
+
+    .login-card-children {
+        width: 60%;
+        margin: 20px auto;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .login-card-children > .form-login-card > .user-input,
+    .login-card-children > .form-login-card > .email-input {
+        margin: 5px 0px 10px 0px;
+    }
+
+    /* .container > div:first-child {
         text-align: center;
         margin-bottom: 20px;
     }
@@ -98,5 +123,5 @@ import { authenticateUser } from '@/services/authenticateUser';
 
     .form > p {
         margin-bottom: 5px;
-    }
+    } */
 </style>
