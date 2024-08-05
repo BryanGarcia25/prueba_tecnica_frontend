f3864a90fb6bfdb74fe614fdb52e22e18f8823b2
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-08-05 02:09:31 -0700

    Agregando mensajes de confirmación al eliminar usuario y se modifica la tabla

--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,23 +1,25 @@
 <template>
-    <div>
+    <div class="container">
         <h1>Lista de usuarios registrados</h1>
-        <el-table :data="users" border style="width: 100%">
-            <el-table-column fixed prop="id" label="ID" width="50" />
-            <el-table-column prop="name" label="Nombre" width="200" />
-            <el-table-column prop="email" label="Correo Electronico" width="200" />
-            <el-table-column prop="contacts[0]['phone']" label="Teléfono" width="120" />
-            <el-table-column prop="contacts[0]['references']" label="Referencias" width="250" />
-            <el-table-column prop="createDate" label="Fecha de registro" width="150" />
-            <el-table-column fixed="right" label="Operaciones" min-width="20">
-            <template v-slot="userInfo">
-                <el-button type="primary" size="default" @click="userDetails(userInfo.row)">Detalles</el-button>
-                <el-button type="primary" size="default" @click="editUser(userInfo.row)">Editar</el-button>
-                <el-button type="danger" size="default" @click="removeUser(userInfo.row)">Eliminar</el-button>
-            </template>
-            </el-table-column>
-        </el-table>
+        <hr>
+        <div class="table-users">
+            <el-table :data="users" border style="width: 100%">
+                <el-table-column fixed prop="id" label="ID" width="50" />
+                <el-table-column prop="name" label="Nombre" width="180" />
+                <el-table-column prop="email" label="Correo Electronico" width="200" />
+                <el-table-column prop="contacts[0]['phone']" label="Teléfono" width="120" />
+                <el-table-column prop="contacts[0]['references']" label="Referencias" width="225" />
+                <el-table-column prop="createDate" label="Fecha de registro" width="200" />
+                <el-table-column fixed="right" label="Operaciones" min-width="285">
+                <template v-slot="userInfo">
+                    <el-button type="primary" size="default" @click="userDetails(userInfo.row)">Detalles</el-button>
+                    <el-button type="primary" size="default" @click="editUser(userInfo.row)">Editar</el-button>
+                    <el-button type="danger" size="default" @click="removeUser(userInfo.row)">Eliminar</el-button>
+                </template>
+                </el-table-column>
+            </el-table>
+        </div>
         <el-button type="primary" size="default" @click="createUser()">Crear nuevo usuario</el-button>
-        
     </div>
 </template>
 
@@ -25,8 +27,9 @@
     import router from '@/router';
     import { getUsers } from '@/services/getUsers';
     import { removeUserById } from '@/services/removeUserById';
+    import { ElMessage, ElMessageBox } from 'element-plus';
     import { ref } from 'vue';
-    
+
     const users = ref();
     getUsers().then(resp => {
         users.value = resp;
@@ -45,11 +48,37 @@
     }
 
     const removeUser = (user) => {
-        removeUserById(user.id);
+        ElMessageBox.confirm(
+            "¿Estas seguro que quieres eliminar este usuario?",
+            "Atención",
+            {
+                confirmButtonText: 'Eliminar',
+                cancelButtonText: 'Cancelar',
+                type: 'warning'
+            }
+        ).then(() => {
+            removeUserById(user.id);
+            ElMessage({
+                type: 'success',
+                message: 'Usuario eliminado'
+            });
+            router.push('/usuarios')
+        }).catch(() => {
+            ElMessage({
+                type: 'info',
+                message: 'Usuario no eliminado'
+            });
+        })
     }
 
 </script>
 
 <style scoped>
+    .container {
+        padding: 20px 10px;
+    }
 
+    .table-users {
+        margin: 30px 0;
+    }
 </style>
